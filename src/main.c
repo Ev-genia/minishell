3ffd6a8ce91f198065c23b5f5d810fc0ea79ee11
Wendell Collen <wcollen@un-a3.kzn.21-school.ru>
2022-06-30 15:48:16 +0300

    changes in ft_shell

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: wcollen <wcollen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:44:48 by mlarra            #+#    #+#             */
-/*   Updated: 2022/06/30 15:23:52 by wcollen          ###   ########.fr       */
+/*   Updated: 2022/06/30 15:47:34 by wcollen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 
 void    ft_shell()
 {
-    
+    int i;
+
+    i = 0;
 }
 
 int main(int argc, char **argv, char **env)
