d81626bca274ee84ac0e5921077bc3c0d93aab6c
Milagro Larra <mlarra@un-a5.kzn.21-school.ru>
2022-06-30 12:58:42 +0300

    2022-06-30

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:44:48 by mlarra            #+#    #+#             */
-/*   Updated: 2022/06/30 12:55:52 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/06/30 12:58:06 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **env)
 {
     if (ft_parse(argc, argv))
         return (ft_error());
