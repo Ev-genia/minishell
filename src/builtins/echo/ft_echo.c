faf47c238ede62a859b8768fb97ddbc6d805be62
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-07-13 15:07:12 +0300

    update ft_echo.c

--- src/builtins/echo/ft_echo.c
+++ src/builtins/echo/ft_echo.c
@@ -6,30 +6,20 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 10:16:58 by mlarra            #+#    #+#             */
-/*   Updated: 2022/07/13 14:55:27 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/07/13 15:06:44 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <minishell.h>
 #include "../../../includes/minishell.h"
 
-int	ft_count_arg_echo(char **arg)
-{
-	int	i;
-
-	i = 0;
-	while (arg[i] != NULL)
-		i++;
-	return (i);
-}
-
 void	ft_echo(char **arg)
 {
 	int	i;
 
-	i = 0;
-	if (ft_count_arg_echo(arg) == 1)
-		printf("%s\n", arg[i]);
-	else
+	i = 1;
+	if (ft_strncmp(arg[i], "-n", 2) == 0)
 		printf("%s", arg[++i]);
+	else
+		printf("%s\n", arg[i]);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:44:48 by mlarra            #+#    #+#             */
-/*   Updated: 2022/07/13 14:56:41 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/07/13 15:06:11 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,23 @@ int	main(int argc, char **argv, char **env)
 	test_env = ft_env_struct(env);
 	ft_env(test_env);
 */
-/*
+// /*
 	//для тестирования echo
-	char	*test1[2];
-	char	*test2[3];
+	char	*test1[3];
+	char	*test2[4];
 
 	(void)argc;
 	(void)argv;
 	(void)env;
-	test1[0] = "abracadabra\n";
-	test1[1] = NULL;
-	test2[0] = "-n";
-	test2[1] = "testTESTtest|";
-	test2[2] = NULL;
+	test1[0] = "echo";
+	test1[1] = "abracadabra\n";
+	test1[2] = NULL;
+	test2[0] = "echo";
+	test2[1] = "-n";
+	test2[2] = "testTESTtest|";
+	test2[3] = NULL;
 	ft_echo(test2);
 	ft_echo(test1);
-*/
+// */
 	return (0);
 }
