60ab40afcc10eb498e95410dff8d2e60413c69f9
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-07-15 14:43:02 +0300

    add check to export

--- src/builtins/export/ft_export.c
+++ src/builtins/export/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:11:39 by mlarra            #+#    #+#             */
-/*   Updated: 2022/07/15 13:43:53 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/07/15 14:41:38 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,15 +56,30 @@ void	ft_print_export(t_env *export)
 	}
 }
 
+void	ft_no_valid_export(char *str)
+{
+	ft_putstr_fd("export: `", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd(": not a valid identifier\n", 2);
+}
+
 void	ft_export(char **arg, t_env *export)
 {
 	int	i;
+	int	j;
 
 	i = 0;
+	j = 0;
 	if (ft_arr_len(arg) != 1)
 	{
 		while (arg[++i] != NULL)
+		{
+			if (j == 0 && (ft_isdigit(arg[i][j]) || !ft_isalpha(arg[i][j])))
+				return (ft_no_valid_export(arg[i]));
+			if (!ft_isdigit(arg[i][j]) && !ft_isalpha(arg[i][j]))
+				return (ft_no_valid_export(arg[i]));
 			ft_add_to_export(&export, arg[i]);
+		}
 		export = ft_sorted_export(export);
 	}
 	ft_print_export(export);
