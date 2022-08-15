81ac7b52792f716d05c8c99e7ecd408971adfa74
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-08-15 11:29:49 +0300

    2022-08-12

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:44:48 by mlarra            #+#    #+#             */
-/*   Updated: 2022/08/12 16:09:53 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/08/12 17:31:24 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,16 @@ char	*ft_readline(const char *prompt)
 	 return (str);
 }
 
+void	ft_wait()
+{
+	int	status;
+	int	i;
+
+	i = 0;
+	while (waitpid(-1, &status, 0))
+		i++;
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	t_set	set;
@@ -53,16 +63,35 @@ int	main(int argc, char **argv, char **env)
 	ft_init_set(&set, env);
 	ft_init_f(set.func);
 	ft_init_arr(set.choice_func);
+
+set.lst_cmds = malloc(sizeof(t_cmd));
+set.lst_cmds->lst_args = ft_lstnew("ls");
+set.lst_cmds->file_read = NULL;
+set.lst_cmds->file_write = NULL;
+set.lst_cmds->flag_heredoc_read = 0;
+set.lst_cmds->flag_heredoc_write = 0;
+set.lst_cmds->flag_pipe = 0;
+set.lst_cmds->flag_redir_read = 0;
+set.lst_cmds->flag_redir_write = 0;
+set.lst_cmds->limiter = NULL;
+set.lst_cmds->sets = &set;
+set.lst_cmds->next = NULL;
+
 	while (status == 0)
 	{
-		ft_signal_parent_process();
-		ft_term_caps(&set);//перевод терминала в неканонический режим
-		str = ft_readline("\033[36mminishell(→_→)$\033[0m ");
+		// ft_signal_parent_process();
+		// ft_term_caps(&set);//перевод терминала в неканонический режим
+		str = ft_readline("\033[36m(→_→)$\033[0m ");
 		signal(SIGQUIT, ft_handler);
-		tcsetattr(0, TCSANOW, &set.orig_term);//перевод терминала в канонический режим
+		// tcsetattr(0, TCSANOW, &set.orig_term);//перевод терминала в канонический режим
 		if (!str)
 			exit(0);
-		set.lst_cmds = ft_parse(str, &set);
+		while (set.lst_cmds)
+		{
+			ft_command(*(set.lst_cmds));
+			set.lst_cmds = set.lst_cmds->next;
+		}
+		// set.lst_cmds = ft_parse(str, &set);
 
 		// if (!set.lst_cmds)
 		// {
@@ -82,7 +111,7 @@ int	main(int argc, char **argv, char **env)
 
 		free(str);
 	}
-	//ft_wait();
+	ft_wait();
 
 	ft_lstclear_env(&set.enpv);
 	ft_lstclear_env(&set.export);
