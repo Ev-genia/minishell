9a31dd66573188a70940c0e6bcb8209f952d5bff
Wcollen <wcollen@student.21-school.ru>
2022-09-26 14:00:46 +0300

    main.c printf

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: wcollen <wcollen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:44:48 by mlarra            #+#    #+#             */
-/*   Updated: 2022/09/26 13:07:36 by wcollen          ###   ########.fr       */
+/*   Updated: 2022/09/26 14:00:04 by wcollen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,8 +78,8 @@ int	main(int argc, char **argv, char **env)
 	while (1)
 	{
 		// signal(SIGINT, ft_signal_ctrl_c);
-		str = //ft_strdup("cat d1 d2");//("echo \"$USER\"");
-		ft_readline("\033[36m(→_→)$\033[0m ");
+		str = ft_strdup("cat d1 d2");//("echo \"$USER\"");
+		//ft_readline("\033[36m(→_→)$\033[0m ");
 		set.lst_cmds = ft_parse(str, &set);
 		// signal(SIGTSTP, SIG_DFL);
 		// dup2(set.start_fd_in, 0);
@@ -93,17 +93,6 @@ int	main(int argc, char **argv, char **env)
 		// dup2(set.start_fd_in, 0);
 		// signal(SIGINT, SIG_IGN);
 		// while (set.lst_cmds)
-//t_cmd *tmp = set.lst_cmds;
-// while(tmp)
-// {
-// 	while(tmp->lst_args)
-// 	{
-// 		printf("ARG %s\n", (char *)tmp->lst_args->content);
-// 		tmp->lst_args = tmp->lst_args->next;
-// 	}
-// 	tmp = tmp->next;
-// }
- printf("ARG_OLD %s\n", (char *)set.lst_cmds->lst_args->content);
 
 		while(set.lst_cmds)
 		{
